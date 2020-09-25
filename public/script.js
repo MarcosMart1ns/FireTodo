ccc75980576224a7baef1beb70e2196de8996554
Marcos Martins <marcos_36ma@hotmail.com>
2020-09-24 22:00:52 -0300

    added edit button

--- public/script.js
+++ public/script.js
@@ -1,11 +1,14 @@
 const db = firebase.database();
 const data = db.ref('docs/')
 
+
 function createToDo(){
-    const title = document.getElementById('create-Todo-Title').value;
-    const content = document.getElementById('todo-text').value;
-    create(title,content);
-    //renderToDo(title,content);
+    let title = document.getElementById('create-Todo-Title');
+    let content = document.getElementById('todo-text');
+    create(title.value,content.value);
+
+    title.value = ''
+    content.value = ''
 }
 
 function deleteTodo(){
@@ -22,39 +25,42 @@ function renderToDo(title,content,id){
     let todoTitle = document.createElement('h3');
     let todoContent = document.createElement('p');
     let btn = document.createElement('button');
+    let btnEdit = document.createElement('button');
+    
 
-    //atribui as classes individualmente
+    //atribui as classes individualmente aos elementos
     todo.className ='todo-item';
     todoTitle.className = 'todo-title';
     todoContent.className = 'todo-content';
     btn.className = 'btn'
+    btnEdit.className = 'btn'
     todo.id = id
     
-    //add conteudo nelas
+    //preenche os elementos
     todoTitle.innerText = title;
     todoContent.innerHTML = content;
     btn.innerHTML = 'Excluir';
+    btnEdit.innerHTML = 'Editar';
+
+    //atribui a função deletarTodos no botão
     btn.onclick = deleteInDB
+    btnEdit.onclick = update
         
 
-    //adiciona elas no todo-item
+    //adiciona os elementos no todo-item
     todo.appendChild(todoTitle);
     todo.appendChild(todoContent);
+    todo.appendChild(btnEdit);
     todo.appendChild(btn);
 
-    //adiciona todo-item no list
+    //adiciona todo-item no list de todos
     listContainer.appendChild(todo);
 
-
-    // <div class = 'todo-item'>
-    //     <h3 class = 'todo-title'>Um todo com nome inesquecível</h3>
-    //     <p class = 'todo-content'>Uma lamentação bem grande pois estou sem criatividade no momento</p>
-    // </div>
-    
 }
 
-//Database CRUD no Firebase
+//Operações no firebase 
 
+//Create
 async function create (title,content){
     if(title.length === 0){
         title = 'untitled';
@@ -69,27 +75,49 @@ async function create (title,content){
 
 }
 
+// Read
 async function read (){
 
-    data.on("child_added",snap =>{
+    await data.on("child_added",snap =>{
        let results = snap.val();
         
        renderToDo(results.title,results.content,snap.key);
     })
 }
 
+//Update
+async function update(){
+    const thisElement = this.parentElement;
+    const id = thisElement.id
+    const todoItem = document.querySelector(`#${id}`)
+    
+    let title = document.querySelector(`#${id} .todo-title`);
+    let content = document.querySelector(`#${id} .todo-content`)
+
+    let inputTitle = document.createElement('input')
+    let inputContent = document.createElement('input')
+
+    inputTitle.value = title.innerHTML;
+    inputContent.value = content.innerHTML;
+    
+    todoItem.appendChild(inputTitle);
+    todoItem.appendChild(inputContent);
+
+    title.remove();
+    content.remove();
+
+}
+
+//Delete
 async function deleteInDB(){
-    let thisElement = this.parentElement;
-    let id = thisElement.id
+    const thisElement = this.parentElement;
+    const id = thisElement.id
+
     let ref = db.ref('docs/'+id)
     let todo_item = document.getElementById(`${id}`);
 
     await ref.remove();
     todo_item.remove();
-
 }
 
-async function update(){
-    return alert();
-}
 
---
+++ public/scripts/config.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDfcCX5JAQr0x9XAquNTyS4fFqZ9ZL9M2c",
+    authDomain: "marcos-todo.firebaseapp.com",
+    databaseURL: "https://marcos-todo.firebaseio.com",
+    projectId: "marcos-todo",
+    storageBucket: "marcos-todo.appspot.com",
+    messagingSenderId: "472499826064",
+    appId: "1:472499826064:web:0d6504bb3f50c024161715"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+          
